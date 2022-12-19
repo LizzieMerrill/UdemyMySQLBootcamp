762d640897346387346178bd6120a2aad1c7dcca
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 08:51:11 -0700

    Update Section3CreatingDatabasesAndTables.sql

--- Section3CreatingDatabasesAndTables.sql
+++ Section3CreatingDatabasesAndTables.sql
@@ -1,1 +1,5 @@
 show databases; --shows all databases you've created, and the MySQL default databases
+
+CREATE DATABASE <name>; --creates an empty database with name <name>
+create database <name>; --also creates an empty database with name <name>
+--spaces are technically allowed in database names, but highly frowned upon

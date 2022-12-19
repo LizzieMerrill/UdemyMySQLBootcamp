320fc2067f746985597ee1638aba8598043e2a9c
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 08:58:33 -0700

    Update SQLbasics1.5.sql
    Made comments syntactically valid

--- SQLbasics1.5.sql
+++ SQLbasics1.5.sql
@@ -4,28 +4,28 @@ Using default database on the online IDE for all calls (v1.6)
 */
 
 
---Sorts products by price in descending order
+-- Sorts products by price in descending order
 SELECT * FROM products
 ORDER BY Price DESC;
 
 
---Sorts products by rice in ascending order (default call)
+-- Sorts products by rice in ascending order (default call)
 SELECT * FROM products
 ORDER BY Price;
 
 
---Calls data from two tables, customers and orders, to display a sorted list of customers based on how many orders they've made
-SELECT 	--call begin
-    customerName,	--instructs the first column to display CustomerName information
-    COUNT(*) AS 'number of orders'	--displays customer names sorted based on how many times they ordered
-FROM customers	--call finished
-INNER JOIN orders	--(begin) this call combines data from the customers table and the orders table on the display
-	ON orders.CustomerID = customers.CustomerID	--places 'number of orders' in the second column next to customer name, where
-GROUP BY customers.customerID	--for customers who have the same number of orders, the next level of sorting is by their ID
-ORDER BY "number of orders" DESC;	--most to least orders (call end)
+-- Calls data from two tables, customers and orders, to display a sorted list of customers based on how many orders they've made
+SELECT 	-- call begin
+    customerName,	-- instructs the first column to display CustomerName information
+    COUNT(*) AS 'number of orders'	-- displays customer names sorted based on how many times they ordered
+FROM customers	-- call finished
+INNER JOIN orders	-- (begin) this call combines data from the customers table and the orders table on the display
+	ON orders.CustomerID = customers.CustomerID	-- places 'number of orders' in the second column next to customer name, where
+GROUP BY customers.customerID	-- for customers who have the same number of orders, the next level of sorting is by their ID
+ORDER BY "number of orders" DESC;	-- most to least orders (call end)
 
 
---Instagram clone database calls: this is new content, I haven't learned this up to this point.
+-- Instagram clone database calls: this is new content, I haven't learned this up to this point.
 SELECT 
     username,
     photos.id,
@@ -41,7 +41,7 @@ ORDER BY total DESC
 LIMIT 1;
 
 
---More advanced functions in MySQL; they look familiar, but I have never used them in data before.
+-- More advanced functions in MySQL; they look familiar, but I have never used them in data before.
 SELECT first_name, 
        last_name, 
        Count(rating)                    AS COUNT, 
@@ -59,5 +59,5 @@ FROM   reviewers
 GROUP  BY reviewers.id; 
 
 
---This function deletes the whole customers table
+-- This function deletes the whole customers table
 DROP TABLE Customers;

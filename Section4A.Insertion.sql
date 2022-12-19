89cfafa1b2758210e424f3f524b540bdc180e797
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 11:56:40 -0700

    Create Section4A.Insertion.sql

---
+++ Section4A.Insertion.sql
@@ -0,0 +1,11 @@
+SELECT database();
+/*
++------------+
+| database() |
++------------+
+| pet_shop   |
++------------+
+1 row in set (0.00 sec)
+*/
+CREATE TABLE cats(name VARCHAR(50), age INT);
+-- Query OK, 0 rows affected (0.02 sec)

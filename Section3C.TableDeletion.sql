f6faa8c6c03c652ca8a0e47604e91baf41862381
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 11:26:39 -0700

    Update Section3C.TableDeletion.sql

--- Section3C.TableDeletion.sql
+++ Section3C.TableDeletion.sql
@@ -1,1 +1,15 @@
 -- DROP TABLE <tablename>;
+
+DROP TABLE cats;
+-- Query OK, 0 rows affected (0.01 sec)
+SHOW TABLES;
+/*
++--------------------+
+| Tables_in_pet_shop |
++--------------------+
+| dogs               |
++--------------------+
+1 row in set (0.00 sec)
+*/
+DROP TABLE dogs;
+-- Query OK, 0 rows affected (0.01 sec)

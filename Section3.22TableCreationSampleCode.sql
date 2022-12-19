049f402dcc4e01765a710ee8c642120f2545f5fc
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 10:37:01 -0700

    Create Section3.22TableCreationSampleCode.sql

---
+++ Section3.22TableCreationSampleCode.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cats
+(
+name VARCHAR(100),
+age INT
+); -- creates a table of cats with two columns, one for name and one for age, properly typed and character limited
+
+
+-- IN TERMINAL

ffa498216ef488aa25fc7959da31c336c2ff4286
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 08:57:09 -0700

    Update SQLvsMySQLSection2.sql

--- SQLvsMySQLSection2.sql
+++ SQLvsMySQLSection2.sql
@@ -1,5 +1,5 @@
-SELECT * FROM Users WHERE Age >= 18;  --selects all adult users, or in english, "find all users who are 18 and older"
-quit; --quits the terminal
+SELECT * FROM Users WHERE Age >= 18;  -- selects all adult users, or in english, "find all users who are 18 and older"
+quit; -- quits the terminal
 
---MySQL Workbench tutorial = section 2.10
---access MySQL command terminal through start search, it will be called "MySQL 8.0 Command Line Client"
+-- MySQL Workbench tutorial = section 2.10
+-- access MySQL command terminal through start search, it will be called "MySQL 8.0 Command Line Client"

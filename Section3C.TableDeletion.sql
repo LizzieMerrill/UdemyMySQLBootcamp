d9b7aa2616fd814a250785b339d2f09a16d6b998
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 11:35:22 -0700

    Update Section3C.TableDeletion.sql
    3.30 Exercise pasted in

--- Section3C.TableDeletion.sql
+++ Section3C.TableDeletion.sql
@@ -13,3 +13,39 @@ SHOW TABLES;
 */
 DROP TABLE dogs;
 -- Query OK, 0 rows affected (0.01 sec)
+
+
+
+
+
+-- 3.30 Table Exercise in CLI: Creates, inspects, and deleted a semi-fleshed-out table.
+
+CREATE TABLE pastries; -- doesn't work the same as creating an empty database does: for tables, you must specify columns with a certain syntax.
+-- ERROR 4028 (HY000): A table must have at least one visible column.
+CREATE TABLE pastries(
+    name VARCHAR(50),
+    quantity INT
+    );
+-- Query OK, 0 rows affected (0.02 sec)
+
+SHOW TABLES;
+/*
++--------------------+
+| Tables_in_pet_shop |
++--------------------+
+| pastries           |
++--------------------+
+1 row in set (0.00 sec)
+*/
+SHOW COLUMNS FROM pastries;
+/*
++----------+-------------+------+-----+---------+-------+
+| Field    | Type        | Null | Key | Default | Extra |
++----------+-------------+------+-----+---------+-------+
+| name     | varchar(50) | YES  |     | NULL    |       |
+| quantity | int         | YES  |     | NULL    |       |
++----------+-------------+------+-----+---------+-------+
+2 rows in set (0.00 sec)
+*/
+DROP TABLE pastries;
+-- Query OK, 0 rows affected (0.01 sec)

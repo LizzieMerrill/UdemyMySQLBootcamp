ad182d84d31c918071abb07416ff176776281d75
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 13:07:21 -0700

    Update Section4A.Insertion.sql
    Insert basics

--- Section4A.Insertion.sql
+++ Section4A.Insertion.sql
@@ -9,3 +9,19 @@ SELECT database();
 */
 CREATE TABLE cats(name VARCHAR(50), age INT);
 -- Query OK, 0 rows affected (0.02 sec)
+
+DESC cats;
+/*
++-------+-------------+------+-----+---------+-------+
+| Field | Type        | Null | Key | Default | Extra |
++-------+-------------+------+-----+---------+-------+
+| name  | varchar(50) | YES  |     | NULL    |       |
+| age   | int         | YES  |     | NULL    |       |
++-------+-------------+------+-----+---------+-------+
+2 rows in set (0.00 sec)
+*/
+
+INSERT INTO cats (name, age) VALUES ("Blue Steele", 5);
+-- Query OK, 1 row affected (0.01 sec)
+INSERT INTO cats (name, age) VALUES ("Jenkins", 7);
+-- Query OK, 1 row affected (0.01 sec)

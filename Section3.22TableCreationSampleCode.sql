ab6eb6804dd3be7781304d2592ed3cf4bcae732a
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 10:51:28 -0700

    Update Section3.22TableCreationSampleCode.sql

--- Section3.22TableCreationSampleCode.sql
+++ Section3.22TableCreationSampleCode.sql
@@ -4,5 +4,33 @@ name VARCHAR(100),
 age INT
 ); -- creates a table of cats with two columns, one for name and one for age, properly typed and character limited
 
+-- 
+-- 
+-- 
+-- 
+-- 
 
 -- IN TERMINAL
+SELECT database();
+-- database() \n tea_shop
+SHOW DATABASES;
+-- terminal: shows defaults, chicken_coop, tea_shop
+CREATE DATABASE pet_shop;
+-- terminal: Query OK, 1 row affected (0.00 sec)
+USE pet_shop;
+-- terminal: Database changed
+CREATE TABLE cats (
+     name VARCHAR(50),
+     age INT
+     );
+-- terminal: Query OK, 0 rows affected (0.01 sec)
+
+-- ^this query creates table 'cats' in database 'pet_shop' vecause we used the USE pet_shop; command earlier
+
+-- in workbench now:
+USE pet_shop;
+CREATE TABLE dogs (
+name VARCHAR(50),
+breed VARCHAR(50), -- same as cats table except for added breed
+age INT
+); 

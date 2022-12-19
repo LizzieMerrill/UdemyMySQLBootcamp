12dab2da8dbb61089611b68fd0a737771b681c0c
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-12-19 08:56:15 -0700

    Update Section3CreatingDatabasesAndTables.sql
    Put spaces around comments to prevent errors, pasted database creations from workbench

--- Section3CreatingDatabasesAndTables.sql
+++ Section3CreatingDatabasesAndTables.sql
@@ -1,11 +1,11 @@
-show databases; --shows all databases you've created, and the MySQL default databases
+show databases; -- shows all databases you've created, and the MySQL default databases
 
-CREATE DATABASE <name>; --creates an empty database with name <name>
-create database <name>; --also creates an empty database with name <name>
---spaces are technically allowed in database names, but highly frowned upon
+CREATE DATABASE <name>; -- creates an empty database with name <name>
+create database <name>; -- also creates an empty database with name <name>
+-- spaces are technically allowed in database names, but highly frowned upon
 
---CREATE DATABASE pet_shop;
---CREATE DATABASE SlimeStore
+-- CREATE DATABASE pet_shop;
+-- CREATE DATABASE SlimeStore
 CREATE DATABASE chicken_coop;
 
 --  I created databases (2 in terminal, one in workbench) on my root MySQL save

1e2ca4efe4b656062aa90fbb57f661634de55b87
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2023-01-17 09:55:01 -0700

    Update Section4B.NotNull.sql
    Tested NOT NULL value on cats2 table.

--- Section4B.NotNull.sql
+++ Section4B.NotNull.sql
@@ -36,3 +36,34 @@ SELECT * FROM cats;
 +-------------+------+
 8 rows in set (0.00 sec)
 */
+
+CREATE TABLE cats2
+    (
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+    );
+-- Query OK, 0 rows affected (0.02 sec)
+/*
+INSERT INTO cats2(name) VALUES('Bilbo');
+ERROR 1364 (HY000): Field 'age' doesn't have a default value
+*/
+
+
+-- Now we need to set a default value for these fields if we would rather have a default value than an error (either way is fine).
+-- But first, let's test cats2 with the not null values
+
+SELECT * FROM cats2;
+-- Empty set (0.00 sec)
+
+INSERT INTO cats2(name, age) VALUES('Bilbo', 19);
+-- Query OK, 1 row affected (0.00 sec)
+
+SELECT * FROM cats2;
+/*
++-------+-----+
+| name  | age |
++-------+-----+
+| Bilbo |  19 |
++-------+-----+
+1 row in set (0.00 sec)
+*/

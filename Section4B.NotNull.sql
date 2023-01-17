ba813cb09a86d0d02cf3599e1d06483ede5959fd
Elizabeth Merrill <97261333+LizzieMerrill@users.noreply.github.com>
2023-01-17 09:40:39 -0700

    Update Section4B.NotNull.sql
    Experimented with original cats table in which NULL values were allowed in all fields.

--- Section4B.NotNull.sql
+++ Section4B.NotNull.sql
@@ -1,1 +1,38 @@
+INSERT INTO cats(name) VALUES('Todd');
+-- Query OK, 1 row affected (0.02 sec)
 
+SELECT * FROM cats;
+/*
++-------------+------+
+| name        | age  |
++-------------+------+
+| Blue Steele |    5 |
+| Jenkins     |    7 |
+| Beth        |    2 |
+| Meatball    |    5 |
+| Turkey      |    1 |
+| Potato Face |   15 |
+| Todd        | NULL |
++-------------+------+
+7 rows in set (0.00 sec)
+*/
+
+INSERT INTO cats() VALUES();
+-- Query OK, 1 row affected (0.01 sec)
+
+SELECT * FROM cats;
+/*
++-------------+------+
+| name        | age  |
++-------------+------+
+| Blue Steele |    5 |
+| Jenkins     |    7 |
+| Beth        |    2 |
+| Meatball    |    5 |
+| Turkey      |    1 |
+| Potato Face |   15 |
+| Todd        | NULL |
+| NULL        | NULL |
++-------------+------+
+8 rows in set (0.00 sec)
+*/
